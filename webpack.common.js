d9b611f855a120fa81d88e11b8c160800c6b6fff
reymundadwi <reymunda613@gmail.com>
2021-10-26 14:51:00 +0700

    navbar styling

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -26,6 +27,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "index.html",
             filename: "index.html"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname,'src/assets'),
+                    to: path.resolve(__dirname,'dist/assets')
+                }
+            ]
         })
     ]
 }
