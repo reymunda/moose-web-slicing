abfbf2f27d5ea568de07990b7a919fd1ca27836a
reymundadwi <reymunda613@gmail.com>
2021-11-07 08:11:17 +0700

    Moose works section - custom video player (VideoJS)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
+import 'video.js/dist/video-js.css';
+import videoJs from 'video.js/dist/video.js';
 import './style/style.css';
 console.log("testing");
+
+const player = videoJs("my_video_1");
+$(function(){
+    var $refreshButton = $('#refresh');
+    var $results = $('#css_result');
+    
+    function refresh(){
+      var css = $('style.cp-pen-styles').text();
+      $results.html(css);
+    }
+  
+    refresh();
+    $refreshButton.click(refresh);
+    
+    // Select all the contents when clicked
+    $results.click(function(){
+      $(this).select();
+    });
+  });
+  
