8a04a98c6e8477e4c4d1411793438db173be3ebc
reymundadwi <reymunda613@gmail.com>
2021-11-09 14:35:07 +0700

    add DOM to question toggle & indicator

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import 'video.js/dist/video-js.css';
 import videoJs from 'video.js/dist/video.js';
 import './style/style.css';
 
-let questionBar = document.querySelectorAll('.question-bar');
+let questionBar = document.querySelectorAll('.question-bar'),
+    prevToggleButton = document.querySelector('.prev'),
+    nextToggleButton = document.querySelector('.next'),
+    toggleIndicator = document.querySelectorAll('.indicator'),
+    activeIndicator;
 
 questionBar.forEach(e => {
   e.addEventListener('click',() => {
@@ -14,11 +18,35 @@ questionBar.forEach(e => {
       })  
     }
         // let questionDesc = e.childNodes[3];            
-
       e.classList.toggle('active');
   })
 })
 
+nextToggleButton.addEventListener('click', () => {
+    toggleIndicator.forEach(e => {
+      if(e.classList.contains('toggle-selected')){
+        activeIndicator = e;
+      }
+      // activeIndicator.nextElementSibling.classList.add('toggle-selected');
+    })
+    if(activeIndicator.nextElementSibling != null){
+      activeIndicator.classList.remove('toggle-selected');
+      activeIndicator.nextElementSibling.classList.add('toggle-selected');
+    }
+});
+
+prevToggleButton.addEventListener('click',() => {
+  toggleIndicator.forEach(e => {
+    if(e.classList.contains('toggle-selected')){
+      activeIndicator = e;
+    }
+    // activeIndicator.nextElementSibling.classList.add('toggle-selected');
+  })
+  if(activeIndicator.previousElementSibling != null){
+    activeIndicator.classList.remove('toggle-selected');
+    activeIndicator.previousElementSibling.classList.add('toggle-selected');
+  }
+})
 const player = videoJs("my_video_1");
 $(function(){
     var $refreshButton = $('#refresh');
